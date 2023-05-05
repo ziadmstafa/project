9fc42232cd08ea330d450b862a61fef7e74d284f
ziadmstafa <132617483+ziadmstafa@users.noreply.github.com>
2023-05-06 01:40:29 +0300

    Update mian1.js

--- mian1.js
+++ mian1.js
@@ -32,9 +32,9 @@ window.onscroll = () => {
     delay : 200
     });
     ScrollReveal().reveal('.home-content, .hedeng', { origin: 'top' });
-    ScrollReveal().reveal('.home-img, .services-contenr , .Protofolio-box , .content form', { origin: 'bottom' });
-    ScrollReveal().reveal('.home-content h1, .about-img .bj', { origin: 'left' });
-    ScrollReveal().reveal('.home-content p, .about-content .fwe .bj', { origin: 'right' });
+    ScrollReveal().reveal('.home-img, .services-contenr ,  .content form', { origin: 'bottom' });
+    ScrollReveal().reveal('.home-content h1, .about-img ,.bj', { origin: 'left' });
+    ScrollReveal().reveal('.home-content p, .about-content ,.fwe, .bj', { origin: 'right' });
 
 const typed = new Typed('.multiple-text', {
   strings: ['موسسه صدأ الشرق للخدمات اللوجستية'],
@@ -42,4 +42,36 @@ const typed = new Typed('.multiple-text', {
   backSpeed: 100,
   backDelay: 100,
   loop: true
+});
+// auto slide
+let slideIndex = 0;
+const slides = document.querySelectorAll('.carousel-item');
+setInterval(() => {
+    slides[slideIndex].classList.remove('active');
+    slideIndex = (slideIndex + 1) % slides.length;
+    slides[slideIndex].classList.add('active');
+}, 6000);
+
+// slide to specific item
+const carousel = document.querySelector('#default-carousel');
+carousel.addEventListener('click', (event) => {
+    const slideTo = event.target.dataset.carouselSlideTo;
+    if (slideTo !== undefined) {
+        carousel.querySelector('.carousel-item.active').classList.remove('active');
+        carousel.querySelectorAll('.carousel-item')[slideTo].classList.add('active');
+    }
+});
+
+
+carousel.querySelector('[data-carousel-prev]').addEventListener('click', () => {
+    const activeSlide = carousel.querySelector('.carousel-item.active');
+    const prevSlide = activeSlide.previousElementSibling || activeSlide.parentNode.lastElementChild;
+    activeSlide.classList.remove('active');
+    prevSlide.classList.add('active');
+});
+carousel.querySelector('[data-carousel-next]').addEventListener('click', () => {
+    const activeSlide = carousel.querySelector('.carousel-item.active');
+    const nextSlide = activeSlide.nextElementSibling || activeSlide.parentNode.firstElementChild;
+    activeSlide.classList.remove('active');
+    nextSlide.classList.add('active');
 });
